ba6be34623e1f20e89ca58258ace238d7ebd2539
Tima <dadeev.tim@gmail.com>
2022-06-30 13:41:30 +0700

    feat: add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {FullInput} from "./Components/FullInput";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    let [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+        ]
+    )
+
+    const addMessage = (title: string) => {
+        console.log(title)
+    }
+    return (
+        <div className="App">
+            {/*<div>*/}
+            {/*    <input/>*/}
+            {/*    <button>+</button>*/}
+            {/*</div>*/}
+            <FullInput addMessage={addMessage}/>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/FullInput.tsx
@@ -0,0 +1,24 @@
+import React, {ChangeEvent, useState} from 'react';
+
+type FullInputPropsType = {
+    addMessage: (title: string) => void
+}
+
+export const FullInput = (props: FullInputPropsType) => {
+    let [title, setTitle] = useState('')
+
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const onClickButtonHandler = () => {
+        props.addMessage(title)
+    }
+
+    return (
+        <div>
+            <input onChange={onChangeInputHandler}/>
+            <button onClick={onClickButtonHandler}>+</button>
+        </div>
+    );
+};
